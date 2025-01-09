746286efff843317577dd07bcbdd7da8aadbffe3
chskolymps <olympia.francheska.2@gmail.com>
2025-01-09 23:54:33 +0800

    fixed user-interface take exams section

--- app/(pages)/user-dashboard/components/dashboardSettings/takeExams/page.tsx
+++ app/(pages)/user-dashboard/components/dashboardSettings/takeExams/page.tsx
@@ -93,10 +93,10 @@ const ManageExams = () => {
         {exams.map((exam) => (
           <Card
             key={exam.exam_id}
-            className="w-full bg-[#D7D3BF]/30 dark:bg-[#384B70]/30 shadow-md hover:shadow-lg transition-shadow duration-300"
+            className="w-full bg-[#E5E1DA] dark:bg-[#27374D] shadow-md hover:shadow-lg transition-shadow duration-300"
           >
-            <CardHeader className="bg-[#D7D3BF]/30 dark:bg-[#384B70]/30 p-4">
-              <CardTitle className="text-xl font-bold truncate text-gray-800 dark:text-white">
+            <CardHeader className="bg-[#D7D3BF] dark:bg-[#344C64] p-4">
+              <CardTitle className="text-xl font-bold truncate text-[#74512D] dark:text-[#67C6E3]">
                 {exam.exam_title}
               </CardTitle>
             </CardHeader>
@@ -105,23 +105,23 @@ const ManageExams = () => {
                 {exam.exam_desc}
               </p>
               <div className="flex justify-between text-sm">
-                <span className="font-semibold text-blue-600 dark:text-blue-400">
+                <span className="font-semibold text-[#1B9C85] dark:text-[#27E1C1]">
                   Points: {exam.exam_points}
                 </span>
-                <span className="font-semibold text-blue-600 dark:text-blue-400">
+                <span className="font-semibold text-[#088395] dark:text-[#37B7C3]">
                   {exam.subject}
                 </span>
               </div>
               <p className="text-sm text-gray-600 dark:text-gray-300">
                 By: {exam.exam_created_by}
               </p>
-              <p className="text-sm font-medium text-blue-700 dark:text-blue-300">
+              <p className="text-sm font-medium text-[#A66E38] dark:text-[#D8F8B7]">
                 Due: {convertToPHT(exam.deadline)}
               </p>
             </CardContent>
-            <CardFooter className="bg-[#D7D3BF]/30 dark:bg-[#384B70]/30 p-4">
+            <CardFooter className="bg-[#D7D3BF] dark:bg-[#344C64] p-4">
               <Button
-                className="w-full py-2 bg-[#8E806A]/70 hover:bg-[#8E806A] dark:bg-[#508C9B]/50 dark:hover:bg-[#508C9B]/20 text-gray-900 dark:text-white font-semibold transition-colors duration-300"
+                className="w-full py-2 bg-[#8E806A]/60 hover:bg-[#8E806A] dark:bg-[#577B8D]/50 dark:hover:bg-[#577B8D] text-gray-900 dark:text-white font-semibold transition-colors duration-300"
                 onClick={() => handleTakeExam(exam.exam_id)}
                 disabled={loadingExamId === exam.exam_id}
               >
