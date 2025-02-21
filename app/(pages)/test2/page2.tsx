d45d7b69f49a5bc112f8e6002c50046f30e8aca5
Cheska Olympia <olympia.francheska.2@gmail.com>
2025-02-21 17:33:16 +0800

    changes

--- app/(pages)/test2/page2.tsx
+++
@@ -1,9 +0,0 @@
-import Evaluator from "./evaluator2";
-
-export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900">
-      <Evaluator />
-    </div>
-  );
-}
